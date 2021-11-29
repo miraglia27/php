21d2dd07755d032506f9a7bbe5e40bc806706151
miraglia27 <89364600+miraglia27@users.noreply.github.com>
2021-11-28 21:58:25 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+print ("Macarena Miraglia ");
+
+echo "<br>";
+
+var_dump("33");
+
+echo "<br>";
+
+$hobbie = "Andar en bici";
+echo $hobbie;
+
+echo "<br>";
+
+$editor = "Visual Studio";
+echo $editor;
+
+
+$sistema = "Sistema operativo de 64 bits";
+echo "<br>";
+echo $sistema;
+
+?>
